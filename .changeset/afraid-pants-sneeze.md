8f2e0bd00faaec2351738748231f3407ec67f69c
noClaps <sorter_hybrid_07@icloud.com>
2024-07-31 20:10:00 +0100

    Properly trim blockquote lines

---
+++ .changeset/afraid-pants-sneeze.md
@@ -0,0 +1,5 @@
+---
+"@noclaps/znak": patch
+---
+
+Properly trim blockquote lines
--- src/parser/blockquotes.ts
+++ src/parser/blockquotes.ts
@@ -3,7 +3,7 @@ import parser from "./index";
 export default function blockquotes(input: string): Token {
   const lines = input
     .split("\n")
-    .map((line) => line.slice(1).trimEnd())
+    .map((line) => line.slice(1).trim())
     .join("\n");
 
   return { element: "blockquote", contents: parser(lines) };
