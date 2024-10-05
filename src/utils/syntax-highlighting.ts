a157637a09dfbcd60f85f470bdfd5f934f959b05
noClaps <04plugs-bios@icloud.com>
2024-10-05 16:58:02 +0100

    refactor: Simplify syntax highlighter code

--- src/utils/syntax-highlighting.ts
+++ src/utils/syntax-highlighting.ts
@@ -1,32 +1,14 @@
 import {
-	createHighlighterCoreSync,
 	type BundledLanguage,
+	bundledLanguages,
 	type BundledTheme,
-	bundledLanguagesInfo,
-	type LanguageRegistration,
-	type MaybeArray,
-	type ThemeRegistration,
-	bundledThemesInfo,
+	bundledThemes,
+	createHighlighter,
 } from "shiki";
-import { createOnigurumaEngine } from "shiki/engine/oniguruma";
 
-const langs: MaybeArray<LanguageRegistration>[] = [];
-for (const bl of bundledLanguagesInfo) {
-	const { default: lang } = await bl.import();
-	langs.push(lang);
-}
-
-const themes: MaybeArray<ThemeRegistration>[] = [];
-for (const bt of bundledThemesInfo) {
-	const { default: theme } = await bt.import();
-	themes.push(theme);
-}
-
-const engine = await createOnigurumaEngine(import("shiki/wasm"));
-const shiki = createHighlighterCoreSync({
-	langs,
-	themes,
-	engine,
+const shiki = await createHighlighter({
+	themes: Object.keys(bundledThemes),
+	langs: Object.keys(bundledLanguages),
 });
 
 export function highlightSyntax(
