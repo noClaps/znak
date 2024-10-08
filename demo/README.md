696a727b8746ec5b556ff7c5a89758f106a5b498
noClaps <04plugs-bios@icloud.com>
2024-10-08 22:46:37 +0100

    feat: Add demo site

---
+++ demo/README.md
@@ -0,0 +1,27 @@
+# Znak Demo Site
+
+This site can be used to show off or test Znak features.
+
+## Build instructions
+
+1. Clone the repository.
+
+   ```sh
+   git clone https://gitlab.com/noClaps/znak-lang.git
+   cd znak-lang/demo
+   ```
+
+2. Install dependencies and start the dev server.
+
+   ```sh
+   bun install
+   make dev
+   ```
+
+3. Build the site.
+
+   ```sh
+   make build
+   ```
+
+   This will build the site and place the built files in `dist/`.
---
+++ demo/src/script.ts
@@ -0,0 +1,24 @@
+import Znak from "../../index.ts";
+
+const inputElem = document.querySelector<HTMLTextAreaElement>("#input");
+if (!inputElem) throw new Error("Input element not found");
+
+const outputElem = document.querySelector("output");
+if (!outputElem) throw new Error("Output element not found");
+
+inputElem.addEventListener("input", async () => {
+  outputElem.innerHTML = new Znak(inputElem.value).renderToHTML();
+});
+
+inputElem.addEventListener("keydown", (e) => {
+  if (e.key === "Tab") {
+    e.preventDefault();
+    const start = inputElem.selectionStart;
+    const end = inputElem.selectionEnd;
+    const value = inputElem.value;
+
+    inputElem.value = value.substring(0, start) + "\t" + value.substring(end);
+    inputElem.selectionStart = start + 1;
+    inputElem.selectionEnd = end + 1;
+  }
+});
