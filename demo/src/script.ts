1ad4822c56c11d16348847b7626033fb09d30cef
noClaps <04plugs-bios@icloud.com>
2024-10-09 01:07:55 +0100

    style(demo): Switch to using tabs for indentation

--- demo/src/script.ts
+++ demo/src/script.ts
@@ -7,18 +7,18 @@ const outputElem = document.querySelector("output");
 if (!outputElem) throw new Error("Output element not found");
 
 inputElem.addEventListener("input", async () => {
-  outputElem.innerHTML = new Znak(inputElem.value).renderToHTML();
+	outputElem.innerHTML = new Znak(inputElem.value).renderToHTML();
 });
 
 inputElem.addEventListener("keydown", (e) => {
-  if (e.key === "Tab") {
-    e.preventDefault();
-    const start = inputElem.selectionStart;
-    const end = inputElem.selectionEnd;
-    const value = inputElem.value;
+	if (e.key === "Tab") {
+		e.preventDefault();
+		const start = inputElem.selectionStart;
+		const end = inputElem.selectionEnd;
+		const value = inputElem.value;
 
-    inputElem.value = value.substring(0, start) + "\t" + value.substring(end);
-    inputElem.selectionStart = start + 1;
-    inputElem.selectionEnd = end + 1;
-  }
+		inputElem.value = value.substring(0, start) + "\t" + value.substring(end);
+		inputElem.selectionStart = start + 1;
+		inputElem.selectionEnd = end + 1;
+	}
 });
