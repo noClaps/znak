8dc257d9e51c663a2939660a6ace7aa8193e88a0
noClaps <04plugs-bios@icloud.com>
2024-10-27 18:14:38 +0000

    refactor: Inline blockquotes and headings functions

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,220 +1,236 @@
 import type { CodeTheme } from "../../index.ts";
 import { renderMath } from "../utils/math.ts";
 import { Slugger } from "../utils/slugger.ts";
-import { blockquotes } from "./blockquotes.ts";
 import { codeBlock } from "./code-block.ts";
 import { containers } from "./containers.ts";
-import { headings } from "./headings.ts";
 import { images } from "./images.ts";
 import { inlineFormatting } from "./inline-formatting.ts";
 import { orderedListItems, unorderedListItems } from "./list-items.ts";
 import { tables } from "./tables.ts";
 
 export function parseHeadings(input: string) {
-	const slugger = new Slugger();
+  const slugger = new Slugger();
 
-	const matches = input.matchAll(/^(#{1,6}) (.+)/gm);
-	for (const match of matches) {
-		const level = match[1].length;
-		const heading = match[2];
-		slugger.slug(heading, level);
-	}
+  const matches = input.matchAll(/^(#{1,6}) (.+)/gm);
+  for (const match of matches) {
+    const level = match[1].length;
+    const heading = match[2];
+    slugger.slug(heading, level);
+  }
 
-	return slugger.headings;
+  return slugger.headings;
 }
 
 export async function parse(input: string, codeTheme: CodeTheme) {
-	const slugger = new Slugger();
-	const lines = input.trim().split("\n");
-	const tokens: (HastElement | HastText)[] = [];
-	let buffer = "";
+  const slugger = new Slugger();
+  const lines = input.trim().split("\n");
+  const tokens: (HastElement | HastText)[] = [];
+  let buffer = "";
 
-	for (let lineCursor = 0; lineCursor < lines.length; lineCursor++) {
-		// Headings
-		if (lines[lineCursor].match(/^#{1,6} .+/gm)) {
-			tokens.push(headings(lines[lineCursor], slugger));
-			continue;
-		}
+  for (let lineCursor = 0; lineCursor < lines.length; lineCursor++) {
+    // Headings
+    if (lines[lineCursor].match(/^#{1,6} .+/gm)) {
+      const match = lines[lineCursor].match(/^(#{1,6}) (.+)/m)!;
+      const level = match[1].length;
+      const content = match[2];
+      const slug = slugger.slug(content, level);
+      tokens.push({
+        type: "element",
+        tagName: `h${level}`,
+        children: inlineFormatting(content),
+        properties: { id: slug },
+      });
+      continue;
+    }
 
-		// Blockquotes
-		if (lines[lineCursor].startsWith(">")) {
-			while (lines[lineCursor] && lines[lineCursor].startsWith(">")) {
-				buffer += `${lines[lineCursor]}\n`;
-				lineCursor++;
-			}
-			tokens.push(await blockquotes(buffer, codeTheme));
-			buffer = "";
-			continue;
-		}
+    // Blockquotes
+    if (lines[lineCursor].startsWith(">")) {
+      while (lines[lineCursor] && lines[lineCursor].startsWith(">")) {
+        buffer += `${lines[lineCursor]}\n`;
+        lineCursor++;
+      }
+      const blockquoteLines = buffer
+        .split("\n")
+        .map((line) => line.slice(1).trim())
+        .join("\n");
 
-		// Horizontal rule
-		if (lines[lineCursor].match(/^-{3,}$/m)) {
-			tokens.push({ type: "element", tagName: "hr", children: [] });
-			buffer = "";
-			continue;
-		}
+      tokens.push({
+        type: "element",
+        tagName: "blockquote",
+        children: await parse(blockquoteLines, codeTheme),
+      });
+      buffer = "";
+      continue;
+    }
 
-		// Images
-		if (lines[lineCursor].startsWith("![") && lines[lineCursor].endsWith(")")) {
-			tokens.push(images(lines[lineCursor]));
-			continue;
-		}
+    // Horizontal rule
+    if (lines[lineCursor].match(/^-{3,}$/m)) {
+      tokens.push({ type: "element", tagName: "hr", children: [] });
+      buffer = "";
+      continue;
+    }
 
-		// Code block
-		if (
-			lines[lineCursor].startsWith("```") &&
-			lines
-				.slice(lineCursor + 1)
-				.find((l) =>
-					l.startsWith(
-						"`".repeat((lines[lineCursor].match(/`/g) || []).length),
-					),
-				)
-				?.endsWith("`")
-		) {
-			const backtickCount = (lines[lineCursor].match(/`/g) || []).length;
+    // Images
+    if (lines[lineCursor].startsWith("![") && lines[lineCursor].endsWith(")")) {
+      tokens.push(images(lines[lineCursor]));
+      continue;
+    }
 
-			// Move inside code block
-			buffer += `${lines[lineCursor]}\n`;
-			lineCursor++;
-			while (!lines[lineCursor].endsWith("`".repeat(backtickCount))) {
-				buffer += `${lines[lineCursor]}\n`;
-				lineCursor++;
-			}
+    // Code block
+    if (
+      lines[lineCursor].startsWith("```") &&
+      lines
+        .slice(lineCursor + 1)
+        .find((l) =>
+          l.startsWith(
+            "`".repeat((lines[lineCursor].match(/`/g) || []).length),
+          ),
+        )
+        ?.endsWith("`")
+    ) {
+      const backtickCount = (lines[lineCursor].match(/`/g) || []).length;
 
-			tokens.push(await codeBlock(buffer, codeTheme));
-			buffer = "";
-			continue;
-		}
+      // Move inside code block
+      buffer += `${lines[lineCursor]}\n`;
+      lineCursor++;
+      while (!lines[lineCursor].endsWith("`".repeat(backtickCount))) {
+        buffer += `${lines[lineCursor]}\n`;
+        lineCursor++;
+      }
 
-		// Ordered list (1., 3 space indentation)
-		if (lines[lineCursor].match(/^\d+\. /m)) {
-			while (
-				(lines[lineCursor] && lines[lineCursor].match(/^(\d+\. |   |\t)/m)) ||
-				lines[lineCursor] === ""
-			) {
-				buffer += `${lines[lineCursor]}\n`;
-				lineCursor++;
-			}
+      tokens.push(await codeBlock(buffer, codeTheme));
+      buffer = "";
+      continue;
+    }
 
-			// Move back a line to start with the next element
-			lineCursor--;
+    // Ordered list (1., 3 space indentation)
+    if (lines[lineCursor].match(/^\d+\. /m)) {
+      while (
+        (lines[lineCursor] && lines[lineCursor].match(/^(\d+\. |   |\t)/m)) ||
+        lines[lineCursor] === ""
+      ) {
+        buffer += `${lines[lineCursor]}\n`;
+        lineCursor++;
+      }
 
-			tokens.push({
-				type: "element",
-				tagName: "ol",
-				children: await orderedListItems(buffer, codeTheme),
-			});
-			buffer = "";
-			continue;
-		}
+      // Move back a line to start with the next element
+      lineCursor--;
 
-		// Unordered list [-, 2 space indentation]
-		if (lines[lineCursor].startsWith("- ")) {
-			while (
-				(lines[lineCursor] && lines[lineCursor].match(/^(- |  |\t)/m)) ||
-				lines[lineCursor] === ""
-			) {
-				buffer += `${lines[lineCursor]}\n`;
-				lineCursor++;
-			}
+      tokens.push({
+        type: "element",
+        tagName: "ol",
+        children: await orderedListItems(buffer, codeTheme),
+      });
+      buffer = "";
+      continue;
+    }
 
-			// Move back a line to start with the next element
-			lineCursor--;
+    // Unordered list [-, 2 space indentation]
+    if (lines[lineCursor].startsWith("- ")) {
+      while (
+        (lines[lineCursor] && lines[lineCursor].match(/^(- |  |\t)/m)) ||
+        lines[lineCursor] === ""
+      ) {
+        buffer += `${lines[lineCursor]}\n`;
+        lineCursor++;
+      }
 
-			tokens.push({
-				type: "element",
-				tagName: "ul",
-				children: await unorderedListItems(buffer, codeTheme),
-			});
-			buffer = "";
-			continue;
-		}
+      // Move back a line to start with the next element
+      lineCursor--;
 
-		// Tables
-		if (lines[lineCursor].startsWith("| ")) {
-			while (lines[lineCursor] && lines[lineCursor].startsWith("| ")) {
-				buffer += `${lines[lineCursor]}\n`;
-				lineCursor++;
-			}
+      tokens.push({
+        type: "element",
+        tagName: "ul",
+        children: await unorderedListItems(buffer, codeTheme),
+      });
+      buffer = "";
+      continue;
+    }
 
-			tokens.push(tables(buffer));
-			buffer = "";
-			continue;
-		}
+    // Tables
+    if (lines[lineCursor].startsWith("| ")) {
+      while (lines[lineCursor] && lines[lineCursor].startsWith("| ")) {
+        buffer += `${lines[lineCursor]}\n`;
+        lineCursor++;
+      }
 
-		// HTML Elements
-		if (lines[lineCursor].startsWith("<")) {
-			buffer += `${lines[lineCursor]}\n`;
-			while (!lines[lineCursor].includes("</")) {
-				lineCursor++;
-				buffer += `${lines[lineCursor]}\n`;
-			}
+      tokens.push(tables(buffer));
+      buffer = "";
+      continue;
+    }
 
-			tokens.push({ type: "text", value: buffer });
-			buffer = "";
-			continue;
-		}
+    // HTML Elements
+    if (lines[lineCursor].startsWith("<")) {
+      buffer += `${lines[lineCursor]}\n`;
+      while (!lines[lineCursor].includes("</")) {
+        lineCursor++;
+        buffer += `${lines[lineCursor]}\n`;
+      }
 
-		// Math block
-		if (
-			lines[lineCursor] === "$$" &&
-			lines.slice(lineCursor + 1).includes("$$")
-		) {
-			for (lineCursor++; lines[lineCursor] !== "$$"; lineCursor++) {
-				buffer += lines[lineCursor];
-			}
+      tokens.push({ type: "text", value: buffer });
+      buffer = "";
+      continue;
+    }
 
-			tokens.push(renderMath(buffer, true));
-			buffer = "";
-			continue;
-		}
+    // Math block
+    if (
+      lines[lineCursor] === "$$" &&
+      lines.slice(lineCursor + 1).includes("$$")
+    ) {
+      for (lineCursor++; lines[lineCursor] !== "$$"; lineCursor++) {
+        buffer += lines[lineCursor];
+      }
 
-		// Container
-		if (
-			lines[lineCursor].startsWith(":::") &&
-			lines
-				.slice(lineCursor + 1)
-				.find((l) =>
-					l.startsWith(
-						":".repeat(
-							(lines[lineCursor].split(" ")[0].match(/:/g) || []).length,
-						),
-					),
-				)
-				?.endsWith(":")
-		) {
-			const colonCount = (lines[lineCursor].split(" ")[0].match(/:/g) || [])
-				.length;
+      tokens.push(renderMath(buffer, true));
+      buffer = "";
+      continue;
+    }
 
-			// Move inside container
-			buffer += `${lines[lineCursor]}\n`;
-			for (
-				lineCursor++;
-				lines[lineCursor] !== ":".repeat(colonCount);
-				lineCursor++
-			) {
-				buffer += `${lines[lineCursor]}\n`;
-			}
+    // Container
+    if (
+      lines[lineCursor].startsWith(":::") &&
+      lines
+        .slice(lineCursor + 1)
+        .find((l) =>
+          l.startsWith(
+            ":".repeat(
+              (lines[lineCursor].split(" ")[0].match(/:/g) || []).length,
+            ),
+          ),
+        )
+        ?.endsWith(":")
+    ) {
+      const colonCount = (lines[lineCursor].split(" ")[0].match(/:/g) || [])
+        .length;
 
-			tokens.push(await containers(buffer, codeTheme));
-			buffer = "";
-			continue;
-		}
+      // Move inside container
+      buffer += `${lines[lineCursor]}\n`;
+      for (
+        lineCursor++;
+        lines[lineCursor] !== ":".repeat(colonCount);
+        lineCursor++
+      ) {
+        buffer += `${lines[lineCursor]}\n`;
+      }
 
-		// Paragraph
-		for (lineCursor; lines[lineCursor]; lineCursor++) {
-			buffer += lines[lineCursor];
-		}
-		if (buffer) {
-			tokens.push({
-				type: "element",
-				tagName: "p",
-				children: inlineFormatting(buffer),
-			});
-		}
-		buffer = "";
-	}
+      tokens.push(await containers(buffer, codeTheme));
+      buffer = "";
+      continue;
+    }
 
-	return tokens;
+    // Paragraph
+    for (lineCursor; lines[lineCursor]; lineCursor++) {
+      buffer += lines[lineCursor];
+    }
+    if (buffer) {
+      tokens.push({
+        type: "element",
+        tagName: "p",
+        children: inlineFormatting(buffer),
+      });
+    }
+    buffer = "";
+  }
+
+  return tokens;
 }
