3c32952b120c93ebe35c6acd7e69b53c26574b39
noClaps <git@zerolimits.dev>
2025-03-03 10:06:20 +0000

    Change bench script from TypeScript to shellscript

---
+++ bench/bench.sh
@@ -0,0 +1,5 @@
+hyperfine "./znak bench/bench.md -t ./theme.toml" -w20
+# znak 0.13.0
+# Benchmark 1: ./znak bench/bench.md -t ./theme.toml
+# Time (mean ± σ):      26.0 ms ±   0.2 ms    [User: 24.3 ms, System: 1.2 ms]
+# Range (min … max):    25.5 ms …  26.6 ms    101 runs
--- bench/bench.ts
+++
@@ -1,7 +0,0 @@
-import { $ } from "bun";
-
-const t0 = Bun.nanoseconds();
-await $`./znak bench/bench.md -t ./theme.toml`.quiet();
-const t1 = Bun.nanoseconds();
-
-console.log(`Time to output: ${(t1 - t0) / (1000 * 1000)}ms`);
