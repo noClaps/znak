121a20ebb44034c210732b77cdcfc71dd26b450b
noClaps <git@zerolimits.dev>
2025-06-20 21:59:39 +0100

    chore: Replace switch-case with if statements

--- internal/parser/renderer.go
+++ internal/parser/renderer.go
@@ -5,31 +5,31 @@ import (
 )
 
 func Renderer(token node) string {
-	switch token.(type) {
-	case text:
-		return token.(text).Value
-	case element:
-		element := token.(element)
-
-		attributesList := ""
-		if element.Properties != nil {
-			for key, val := range element.Properties {
-				attributesList += fmt.Sprintf(` %s="%s"`, key, val)
-			}
-		}
-
-		contents := ""
-		childrenLen := len(element.Children)
-
-		if childrenLen == 0 {
-			return fmt.Sprintf("<%s%s />", element.TagName, attributesList)
-		}
-
-		for _, item := range element.Children {
-			contents += Renderer(item)
-		}
-		return fmt.Sprintf("<%s%s>%s</%s>", element.TagName, attributesList, contents, element.TagName)
+	if text, ok := token.(text); ok {
+		return text.Value
 	}
 
-	panic("[ERROR] Reached unreachable branch in renderer")
+	element, ok := token.(element)
+	if !ok {
+		panic("[ERROR] Reached unreachable branch in renderer")
+	}
+
+	attributesList := ""
+	if element.Properties != nil {
+		for key, val := range element.Properties {
+			attributesList += fmt.Sprintf(` %s="%s"`, key, val)
+		}
+	}
+
+	contents := ""
+	childrenLen := len(element.Children)
+
+	if childrenLen == 0 {
+		return fmt.Sprintf("<%s%s />", element.TagName, attributesList)
+	}
+
+	for _, item := range element.Children {
+		contents += Renderer(item)
+	}
+	return fmt.Sprintf("<%s%s>%s</%s>", element.TagName, attributesList, contents, element.TagName)
 }
