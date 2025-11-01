6b2136d46f1aa61a6cca6f3ec44f2f4668cfadf8
noClaps <git@zerolimits.dev>
2025-11-01 19:41:04 +0000

    html: Add crate docs

--- crates/html/src/lib.rs
+++ crates/html/src/lib.rs
@@ -1,3 +1,11 @@
+//! An HTML parsing and manipulation library.
+//!
+//! You can add this library to your project with:
+//!
+//! ```bash
+//! cargo add --git https://github.com/noClaps/znak html
+//! ```
+
 mod macros;
 mod parser;
 
