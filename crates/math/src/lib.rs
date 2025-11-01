582a1e5642ce1a9dd22e88fc3177a99bcec5bea7
noClaps <git@zerolimits.dev>
2025-11-01 20:07:57 +0000

    math: Add crate docs

--- crates/math/src/lib.rs
+++ crates/math/src/lib.rs
@@ -1,3 +1,11 @@
+//! A simple library to render LaTeX to MathML.
+//!
+//! You can add this library to your project with:
+//!
+//! ```bash
+//! cargo add --git https://github.com/noClaps/znak math
+//! ```
+
 pub use math_core::MathDisplay;
 use math_core::{LatexToMathML, MathCoreConfig};
 
