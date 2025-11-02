80251683123c130c7f897fa4a49572a8a9a64d79
noClaps <git@zerolimits.dev>
2025-11-02 09:04:12 +0000

    highlight: Move add_lang macro inside default_langs function

--- crates/highlight/src/languages.rs
+++ crates/highlight/src/languages.rs
@@ -4,57 +4,6 @@ use tree_sitter_highlight::HighlightConfiguration;
 
 use crate::Highlight;
 
-macro_rules! add_lang {
-    ($self:expr, [$name:expr$(,$alias:expr)*], $lang:expr, $hl:expr, $inj:expr, $loc:expr) => {
-        $self.add_language(
-            &[$name$(,$alias)*],
-            tree_sitter_highlight::HighlightConfiguration::new(
-                $lang.into(),
-                $name,
-                $hl,
-                $inj,
-                $loc,
-            ).unwrap()
-        )
-    };
-    ($self:expr, [$name:expr$(,$alias:expr)*], $lang:expr, $hl:expr, inj=$inj:expr) => {
-        $self.add_language(
-            &[$name$(,$alias)*],
-            tree_sitter_highlight::HighlightConfiguration::new(
-                $lang.into(),
-                $name,
-                $hl,
-                $inj,
-                "",
-            ).unwrap()
-        )
-    };
-    ($self:expr, [$name:expr$(,$alias:expr)*], $lang:expr, $hl:expr, loc=$loc:expr) => {
-        $self.add_language(
-            &[$name$(,$alias)*],
-            tree_sitter_highlight::HighlightConfiguration::new(
-                $lang.into(),
-                $name,
-                $hl,
-                "",
-                $loc,
-            ).unwrap()
-        )
-    };
-    ($self:expr, [$name:expr$(,$alias:expr)*], $lang:expr, $hl:expr) => {
-        $self.add_language(
-            &[$name$(,$alias)*],
-            tree_sitter_highlight::HighlightConfiguration::new(
-                $lang.into(),
-                $name,
-                $hl,
-                "",
-                "",
-            ).unwrap()
-        )
-    };
-}
-
 impl Highlight {
     pub(crate) fn get_language(&self, name: &str) -> Option<&HighlightConfiguration> {
         Some(self.languages.get(name)?.as_ref())
@@ -93,30 +42,77 @@ impl Highlight {
     }
 
     pub(crate) fn default_langs(&mut self) {
+        macro_rules! add_lang {
+            ([$name:expr$(,$alias:expr)*], $lang:expr, $hl:expr, $inj:expr, $loc:expr) => {
+                self.add_language(
+                    &[$name$(,$alias)*],
+                    tree_sitter_highlight::HighlightConfiguration::new(
+                        $lang.into(),
+                        $name,
+                        $hl,
+                        $inj,
+                        $loc,
+                    ).unwrap()
+                )
+            };
+            ([$name:expr$(,$alias:expr)*], $lang:expr, $hl:expr, inj=$inj:expr) => {
+                self.add_language(
+                    &[$name$(,$alias)*],
+                    tree_sitter_highlight::HighlightConfiguration::new(
+                        $lang.into(),
+                        $name,
+                        $hl,
+                        $inj,
+                        "",
+                    ).unwrap()
+                )
+            };
+            ([$name:expr$(,$alias:expr)*], $lang:expr, $hl:expr, loc=$loc:expr) => {
+                self.add_language(
+                    &[$name$(,$alias)*],
+                    tree_sitter_highlight::HighlightConfiguration::new(
+                        $lang.into(),
+                        $name,
+                        $hl,
+                        "",
+                        $loc,
+                    ).unwrap()
+                )
+            };
+            ([$name:expr$(,$alias:expr)*], $lang:expr, $hl:expr) => {
+                self.add_language(
+                    &[$name$(,$alias)*],
+                    tree_sitter_highlight::HighlightConfiguration::new(
+                        $lang.into(),
+                        $name,
+                        $hl,
+                        "",
+                        "",
+                    ).unwrap()
+                )
+            };
+        }
+
         #[cfg(feature = "agda")]
         add_lang!(
-            self,
             ["agda"],
             tree_sitter_agda::LANGUAGE,
             tree_sitter_agda::HIGHLIGHTS_QUERY
         );
         #[cfg(feature = "bash")]
         add_lang!(
-            self,
             ["bash", "shellscript", "shell", "zsh", "sh"],
             tree_sitter_bash::LANGUAGE,
             tree_sitter_bash::HIGHLIGHT_QUERY
         );
         #[cfg(feature = "c")]
         add_lang!(
-            self,
             ["c"],
             tree_sitter_c::LANGUAGE,
             tree_sitter_c::HIGHLIGHT_QUERY
         );
         #[cfg(feature = "cpp")]
         add_lang!(
-            self,
             ["cpp", "c++"],
             tree_sitter_cpp::LANGUAGE,
             &[
@@ -128,21 +124,18 @@ impl Highlight {
         );
         #[cfg(feature = "css")]
         add_lang!(
-            self,
             ["css"],
             tree_sitter_css::LANGUAGE,
             tree_sitter_css::HIGHLIGHTS_QUERY
         );
         #[cfg(feature = "go")]
         add_lang!(
-            self,
             ["go"],
             tree_sitter_go::LANGUAGE,
             tree_sitter_go::HIGHLIGHTS_QUERY
         );
         #[cfg(feature = "haskell")]
         add_lang!(
-            self,
             ["haskell", "hs"],
             tree_sitter_haskell::LANGUAGE,
             tree_sitter_haskell::HIGHLIGHTS_QUERY,
@@ -151,7 +144,6 @@ impl Highlight {
         );
         #[cfg(feature = "html")]
         add_lang!(
-            self,
             ["html"],
             tree_sitter_html::LANGUAGE,
             tree_sitter_html::HIGHLIGHTS_QUERY,
@@ -159,14 +151,12 @@ impl Highlight {
         );
         #[cfg(feature = "java")]
         add_lang!(
-            self,
             ["java"],
             tree_sitter_java::LANGUAGE,
             tree_sitter_java::HIGHLIGHTS_QUERY
         );
         #[cfg(feature = "javascript")]
         add_lang!(
-            self,
             ["javascript", "js", "jsx"],
             tree_sitter_javascript::LANGUAGE,
             &[
@@ -180,21 +170,18 @@ impl Highlight {
         );
         #[cfg(feature = "jsdoc")]
         add_lang!(
-            self,
             ["jsdoc"],
             tree_sitter_jsdoc::LANGUAGE,
             tree_sitter_jsdoc::HIGHLIGHTS_QUERY
         );
         #[cfg(feature = "json")]
         add_lang!(
-            self,
             ["json"],
             tree_sitter_json::LANGUAGE,
             tree_sitter_json::HIGHLIGHTS_QUERY
         );
         #[cfg(feature = "julia")]
         add_lang!(
-            self,
             ["julia", "jl"],
             tree_sitter_julia::LANGUAGE,
             include_str!("../queries/julia/highlights.scm"),
@@ -202,7 +189,6 @@ impl Highlight {
         );
         #[cfg(feature = "ocaml")]
         add_lang!(
-            self,
             ["ocaml"],
             tree_sitter_ocaml::LANGUAGE_OCAML,
             tree_sitter_ocaml::HIGHLIGHTS_QUERY,
@@ -234,7 +220,6 @@ impl Highlight {
         // );
         #[cfg(feature = "php")]
         add_lang!(
-            self,
             ["php"],
             tree_sitter_php::LANGUAGE_PHP,
             tree_sitter_php::HIGHLIGHTS_QUERY,
@@ -246,7 +231,6 @@ impl Highlight {
         );
         #[cfg(feature = "php")]
         add_lang!(
-            self,
             ["php_only"],
             tree_sitter_php::LANGUAGE_PHP_ONLY,
             tree_sitter_php::HIGHLIGHTS_QUERY,
@@ -254,21 +238,18 @@ impl Highlight {
         );
         #[cfg(feature = "python")]
         add_lang!(
-            self,
             ["python", "py"],
             tree_sitter_python::LANGUAGE,
             tree_sitter_python::HIGHLIGHTS_QUERY
         );
         #[cfg(feature = "regex")]
         add_lang!(
-            self,
             ["regex", "regexp"],
             tree_sitter_regex::LANGUAGE,
             tree_sitter_regex::HIGHLIGHTS_QUERY
         );
         #[cfg(feature = "ruby")]
         add_lang!(
-            self,
             ["ruby", "rb"],
             tree_sitter_ruby::LANGUAGE,
             tree_sitter_ruby::HIGHLIGHTS_QUERY,
@@ -276,7 +257,6 @@ impl Highlight {
         );
         #[cfg(feature = "rust")]
         add_lang!(
-            self,
             ["rust", "rs"],
             tree_sitter_rust::LANGUAGE,
             tree_sitter_rust::HIGHLIGHTS_QUERY,
@@ -284,7 +264,6 @@ impl Highlight {
         );
         #[cfg(feature = "scala")]
         add_lang!(
-            self,
             ["scala"],
             tree_sitter_scala::LANGUAGE,
             tree_sitter_scala::HIGHLIGHTS_QUERY,
@@ -292,7 +271,6 @@ impl Highlight {
         );
         #[cfg(feature = "typescript")]
         add_lang!(
-            self,
             ["typescript", "ts"],
             tree_sitter_typescript::LANGUAGE_TYPESCRIPT,
             &[
@@ -309,7 +287,6 @@ impl Highlight {
         );
         #[cfg(feature = "typescript")]
         add_lang!(
-            self,
             ["tsx"],
             tree_sitter_typescript::LANGUAGE_TSX,
             &[
