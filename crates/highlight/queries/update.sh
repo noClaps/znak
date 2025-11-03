2b1696d5a4798a5617f2cc99934a9c6810bfb3fc
noClaps <git@zerolimits.dev>
2025-11-03 16:20:12 +0000

    highlight: Run curl commands in parallel
    Also enable safety flags on curl command

--- crates/highlight/queries/update.sh
+++ crates/highlight/queries/update.sh
@@ -2,7 +2,7 @@ function get() {
     lang=$1
     output_dir="crates/highlight/queries/$lang/"
     mkdir -p $output_dir
-    curl -LO $2 --output-dir $output_dir
+    curl -fsSLO $2 --output-dir $output_dir &
 }
 
 # cpp
@@ -14,3 +14,5 @@ get julia https://github.com/tree-sitter/tree-sitter-julia/raw/refs/heads/master
 get julia https://github.com/tree-sitter/tree-sitter-julia/raw/refs/heads/master/queries/locals.scm
 # php
 get php https://github.com/tree-sitter/tree-sitter-php/raw/refs/heads/master/queries/injections-text.scm
+
+wait
